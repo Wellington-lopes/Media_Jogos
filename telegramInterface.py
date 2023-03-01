d0e14c30bf32dccb58da677e1bc9215677a7d1d4
libianno <ilibianno@gmail.com>
2023-03-01 11:45:12 -0300

    Adiconadas mensagens de futebol a integração com telegram

--- telegramInterface.py
+++ telegramInterface.py
@@ -4,6 +4,7 @@ from pyrogram import Client
 from asyncio import run
 
 import NBA
+import futebol
 import json
 
 load_dotenv()
@@ -15,19 +16,34 @@ app = Client(
     bot_token = getenv('TELEGRAM_BOT_TOKEN')
 )
 
-user1 = 'Libianno'
-user2 = 'pitbulldaCC'
-
-liga, matches = NBA.main()
-strMaches = json.dumps(matches, indent=2)
-
 
 async def main():
+    user1 = 'Libianno'
+    user2 = 'pitbulldaCC'
+
+    liga, matches = NBA.main()
+    strMaches = json.dumps(matches, indent=2)
+
     await app.start()
     await app.send_message(user1, liga)
     await app.send_message(user1, strMaches)
-    await app.send_message(user2, liga)
-    await app.send_message(user2, strMaches)
+    #await app.send_message(user2, liga)
+    #await app.send_message(user2, strMaches)
+
+    run = futebol.main()
+    try:
+        while 1:
+
+            liga, rodada, matches = next(run)
+            strMaches = json.dumps(matches, indent=2)
+
+            await app.send_message(user1, liga)
+            await app.send_message(user1, rodada)
+            await app.send_message(user1, strMaches)
+
+    except StopIteration:
+        pass
+
     await app.stop()
 
 run(main())
