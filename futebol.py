04c93fc227d247d5798a98663d6bd6813552e8fe
libianno <ilibianno@gmail.com>
2023-03-01 11:31:56 -0300

    Adicionado gerador main e sua inteface no programa principal

--- futebol.py
+++ futebol.py
@@ -76,6 +76,7 @@ def calc(nav,link_table, link_fixtures):
 def main():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--log-level=3')
     nav = Chrome(options=chrome_options)
     nav.implicitly_wait(10)
     links = [
